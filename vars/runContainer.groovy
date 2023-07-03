6bc8d768877f3d8830c40433aeebda3899a3de53
nikolozjakhua <99204376+nikolozjakhua@users.noreply.github.com>
2023-07-03 14:07:25 +0400

    Update runContainer.groovy

--- vars/runContainer.groovy
+++ vars/runContainer.groovy
@@ -1,10 +1,6 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String imageName, String branchName, int portNumber) {
     echo "running container"
-    if (env.BRANCH_NAME == 'main') {
-        sh "docker run -d --expose 3000 -p 3000:3000 --name nodemain${env.BUILD_NUMBER} ${DOCKER_IMAGE_MAIN}"
-    } else if (env.BRANCH_NAME == 'dev') {
-        sh "docker run -d --expose 3001 -p 3001:3000 --name nodedev${env.BUILD_NUMBER} ${DOCKER_IMAGE_DEV}"
-    }
+    sh "docker run -d --expose ${portNumber} -p ${portNumber}:3000 --name node${branchName}${env.BUILD_NUMBER} ${imageName}"
 }
