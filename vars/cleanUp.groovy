39bcfa06c0cd42f32ea099fc62f0dc88e046cae4
nikolozjakhua <99204376+nikolozjakhua@users.noreply.github.com>
2023-07-03 14:10:14 +0400

    Update cleanUp.groovy

--- vars/cleanUp.groovy
+++ vars/cleanUp.groovy
@@ -1,20 +1,11 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String branchName) {
     echo 'Cleaning up'
-    if (env.BRANCH_NAME == 'main') {
-        def containerNames = sh(returnStdout: true, script: 'docker ps -a --format "{{.Names}}" --filter "name=nodemain*"').trim()
-        if (containerNames.isEmpty()) {
-            echo 'No previously deployed containers'
-        } else {
-            sh 'docker rm $(docker ps -a --format "{{.Names}}" --filter "name=nodemain*") -f'
-        } 
-    } else if (env.BRANCH_NAME == 'dev') {
-        def containerNames = sh(returnStdout: true, script: 'docker ps -a --format "{{.Names}}" --filter "name=nodedev*"').trim()
-        if (containerNames.isEmpty()) {
-            echo 'No previously deployed containers'
-        } else {
-            sh 'docker rm $(docker ps -a --format "{{.Names}}" --filter "name=nodedev*") -f'
-        }
-    }   
+    def containerNames = sh(returnStdout: true, script: 'docker ps -a --format "{{.Names}}" --filter "name=node${branchName}*"').trim()
+    if (containerNames.isEmpty()) {
+        echo 'No previously deployed containers'
+    } else {
+        sh 'docker rm $(docker ps -a --format "{{.Names}}" --filter "name=node${branchName}*") -f'
+    } 
 }
