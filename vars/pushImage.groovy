10c61e042f0cd1c155b606f27337b791f77f90ef
nikolozjakhua <99204376+nikolozjakhua@users.noreply.github.com>
2023-07-03 14:12:59 +0400

    Update pushImage.groovy

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -1,15 +1,10 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String imageName, String branchName) {
     echo 'Pushing image'
     withCredentials([usernamePassword(credentialsId: 'docker-creds', passwordVariable: 'PASS', usernameVariable: "USER")]) {
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        if (env.BRANCH_NAME == 'main') {
-            sh "docker tag ${DOCKER_IMAGE_MAIN} nikolozjakhua/cicd:main.v1"
-            sh "docker push nikolozjakhua/cicd:main.v1"                    
-        } else if (env.BRANCH_NAME == 'dev') {
-            sh "docker tag ${DOCKER_IMAGE_DEV} nikolozjakhua/cicd:dev.v1"
-            sh "docker push nikolozjakhua/cicd:dev.v1"
-        }
+        sh "docker tag ${imageName} nikolozjakhua/cicd:${branchName}.v1"
+        sh "docker push nikolozjakhua/cicd:${branchName}.v1"                    
     }
 }
